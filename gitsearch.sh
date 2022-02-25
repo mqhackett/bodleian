572ac088c585405650e7de3ebbc335a3c64e554f
mqhackett <mahacket@redhat.com>
2022-02-25 13:37:50 -0500

    add query to get yamls

--- gitsearch.sh
+++ gitsearch.sh
@@ -1,9 +1,8 @@
 # We'll use the `date` command to get the date for "7 days ago"
 date -v-7d '+%Y-%m-%d'
 # => 2013-07-15
-
-curl -G https://api.github.com/search/repositories       \
-  --data-urlencode "q=created:>`date -v-7d '+%Y-%m-%d'`" \
+curl -G https://api.github.com/search/code       \
+  --data-urlencode "q=extension:yaml" \
   --data-urlencode "sort=stars"                          \
   --data-urlencode "order=desc"                          \
   -H "Accept: application/vnd.github.preview"            \
---
+++ stoloyaml.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/search/code\?q=language:yaml+org:stolostron
