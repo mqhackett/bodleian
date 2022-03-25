a6296da6ebe1b2b905833530d19b8c731f222a96
mqhackett <mahacket@redhat.com>
2022-03-25 15:43:22 -0400

    fix some bugs

--- pulls.sh
+++ pulls.sh
@@ -1,13 +1,13 @@
 # gets all the open PRs for the projects listed in the projects array
 
 
- for i in $(cat repos); do 
+for i in $(cat repos); do 
     url="https://api.github.com/repos/stolostron/"$i"/pulls"
-    curl -s -G $url -H "Accept: application/vnd.github.preview" | jq ".[] | {html_url, title, users}"| jq '.title, .html_url, .users' 
-#	done;
+    curl -s -G $url -H "Accept: application/vnd.github.preview" | jq ".[] | {html_url, title}"| jq '.title, .html_url' 
+	done;
 
 # curl -s -G $url -H "Accept: application/vnd.github.preview" |\
-     jq ".[] | {html_url, title, users}"| jq '.title, .html_url, .users' 
+#     jq ".[] | {html_url, title, users}"| jq '.title, .html_url, .users' 
 
 # curl -G https://api.github.com/repos/stolostron/search-collector/pulls       \
 #   --data-urlencode "state=open" \
