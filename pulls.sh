31206114fcc45da98a281c37847a32dcc8cf2472
mqhackett <mahacket@redhat.com>
2022-03-25 15:31:04 -0400

    added a script to get open pull requests

---
+++ pulls.sh
@@ -0,0 +1,20 @@
+# gets all the open PRs for the projects listed in the projects array
+
+
+ for i in $(cat repos); do 
+    url="https://api.github.com/repos/stolostron/"$i"/pulls"
+    curl -s -G $url -H "Accept: application/vnd.github.preview" | jq ".[] | {html_url, title, users}"| jq '.title, .html_url, .users' 
+#	done;
+
+# curl -s -G $url -H "Accept: application/vnd.github.preview" |\
+     jq ".[] | {html_url, title, users}"| jq '.title, .html_url, .users' 
+
+# curl -G https://api.github.com/repos/stolostron/search-collector/pulls       \
+#   --data-urlencode "state=open" \
+#   -H "Accept: application/vnd.github.preview"            \
+#   | jq ".[] | {html_url, title}"
+
+
+
+
+
